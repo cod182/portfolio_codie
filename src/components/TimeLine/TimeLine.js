165c9693c260adf88c11c0117b73462a537ea01a
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-09 15:44:34 +0000

    added more sample data and scroll

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,7 +1,23 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
-import { Section, SectionDivider, SectionSubText, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  CarouselButton,
+  CarouselButtonDot,
+  CarouselButtons,
+  CarouselContainer,
+  CarouselItem,
+  CarouselItemImg,
+  CarouselItemText,
+  CarouselItemTitle,
+  CarouselMobileScrollNode,
+} from './TimeLineStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionSubText,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import { TimeLineData } from '../../constants/constants';
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
@@ -10,54 +26,70 @@ const Timeline = () => {
   const [activeItem, setActiveItem] = useState(0);
   const carouselRef = useRef();
 
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  };
 
   const handleClick = (e, i) => {
     e.preventDefault();
 
     if (carouselRef.current) {
-      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
+      );
 
       scroll(carouselRef.current, scrollLeft);
     }
-  }
+  };
 
   const handleScroll = () => {
     if (carouselRef.current) {
-      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length
+      );
 
       setActiveItem(index);
     }
-  }
+  };
 
-  // // snap back to beginning of scroll when window is resized
-  // // avoids a bug where content is covered up if coming from smaller screen
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     scroll(carouselRef.current, 0);
-  //   }
+  // snap back to beginning of scroll when window is resized
+  // avoids a bug where content is covered up if coming from smaller screen
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    };
 
-  //   window.addEventListener('resize', handleResize);
-  // }, []);
+    window.addEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <Section id='about'>
+    <Section id="about">
       <SectionTitle>Timeline</SectionTitle>
       <SectionText>My Milestones</SectionText>
-      <CarouselContainer ref={carouselRef}>
+      <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
-          {TimeLineData.map((item, i) => (
-            <CarouselMobileScrollNode key={i} final={i === TOTAL_CAROUSEL_COUNT - 1}>
-              <CarouselItem index={i} id={`carosel__item-${i}`} active={activeItem} onClick={(e) => handleClick(e, i)}>
+          {TimeLineData.map((item, index) => (
+            <CarouselMobileScrollNode
+              key={index}
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
+              <CarouselItem
+                index={index}
+                id={`carosel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
                 <CarouselItemTitle>{item.year}</CarouselItemTitle>
+                <CarouselItemText>{item.text}</CarouselItemText>
                 <CarouselItemImg
                   width="208"
                   height="6"
                   viewBox="0 0 208 6"
                   fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     fill-rule="evenodd"
                     clip-rule="evenodd"
@@ -72,7 +104,8 @@ const Timeline = () => {
                       y1="0.5"
                       x2="208"
                       y2="0.500295"
-                      gradientUnits="userSpaceOnUse">
+                      gradientUnits="userSpaceOnUse"
+                    >
                       <stop stop-color="white" />
                       <stop
                         offset="0.79478"
@@ -87,7 +120,22 @@ const Timeline = () => {
           ))}
         </>
       </CarouselContainer>
-    </Section >
+      <CarouselButtons>
+        {TimeLineData.map((item, index) => {
+          return (
+            <CarouselButton
+              key={index}
+              index={index}
+              active={activeItem}
+              onClick={(e) => handleClick(e, index)}
+              type="button"
+            >
+              <CarouselButtonDot active={activeItem} />
+            </CarouselButton>
+          );
+        })}
+      </CarouselButtons>
+    </Section>
   );
 };
 
