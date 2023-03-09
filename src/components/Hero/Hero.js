5e081d9ce86694177566fc2ee8a9ec620d2be600
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-09 14:51:36 +0000

    added hero banner

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row noPadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello <br /> I Am Codie
+      </SectionTitle>
+      <SectionText>Junior Web Developer!</SectionText>
+      <Button
+        target="_blank"
+        onClick={() => (window.location = 'https://www.google.com')}
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
