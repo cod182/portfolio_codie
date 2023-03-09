3f2d73b7146594f9470ef11a7af4f1c62b4cb9d0
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-09 15:22:12 +0000

    added technologies section with content

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,38 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id='tech'>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>I have worked with multiple technologies from backend to design</SectionText>
+    <List>
+      <ListItem>
+        <DiReact size='3rem' />
+        <ListContainer>
+          <ListTitle>Frontend</ListTitle>
+          <ListParagraph>Experience with React.js</ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiFirebase size='3rem' />
+        <ListContainer>
+          <ListTitle>Backend</ListTitle>
+          <ListParagraph>Experience with Node.js and Databases</ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiZend size='3rem' />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>Experience with Figma</ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
